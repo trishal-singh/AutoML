a28f2171c3b2ad940a71d17c2663575f2c5a993a
trishal-singh <trishalsinghhcwheel@gmail.com>
2021-03-04 15:32:34 +0530

    Added Ada Boost

--- automl.py
+++ automl.py
@@ -4,11 +4,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
 st.title("AUTOML")
 st.write("""Select a dataset and start creating models.""")
 dataset =st.sidebar.selectbox(label="Dataset:",options=('Iris',"Breast Cancer","Wine"))
-model=st.sidebar.selectbox(label="Model:",options=("SVM","KNN","Random Forest"))
+model=st.sidebar.selectbox(label="Model:",options=("SVM","KNN","Random Forest","Ada Boost"))
 def load_data(dataset):
     if dataset=='Iris':
         data=datasets.load_iris()
@@ -40,6 +40,13 @@ def select_parameter(clf):
         params=dict()
         params["n_estimators"]=n_estim
         params["max_depth"]=max_depth
+    if clf=="Ada Boost":
+        n_estim=st.sidebar.number_input(label="N Estimators",value=10)
+        lr=st.sidebar.number_input(label="Learning Rate",value=0.1)
+        params=dict()
+        params["n_estimators"]=n_estim
+        params["learning_rate"]=lr
+
 
     return params
 param=select_parameter(model)
@@ -50,6 +57,8 @@ def create_model(clf,params):
         model=SVC(**params)
     if clf=="Random Forest":
         model=RandomForestClassifier(**params)
+    if clf=="Ada Boost":
+        model=AdaBoostClassifier(**params)
     return model
 classifier=create_model(model,param)
 train_x,test_x,train_y,test_y=train_test_split(X,y,test_size=0.2,random_state=4)
